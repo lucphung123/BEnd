411440a87bced5f283004abdfa0053091b3829db
phamdieu2kk <144933004+phamdieu2kk@users.noreply.github.com>
2024-01-19 13:23:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
@@ -17,10 +17,12 @@ mongoose.connect(process.env.MONGODB_URL, () => {
   console.log("Connected to MongoDB");
 });
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(helmet());
 app.use(cors());
 app.use(morgan("common"));
+app.use(express.static('public'))
 
 //ROUTES
 app.use("/v1/author", authorRoute);
